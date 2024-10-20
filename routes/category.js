a7e60e077c0430d194ec8edc7a7b45945cb7886d
Ajay-singh-github <ajaysikata@gmail.com>
2024-10-20 22:08:39 +0530

    .

---
+++ routes/category.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var router = express.Router()
+var pool = require('./pool')
+var upload=require('./multer');
+
+router.get('/fetch_category', function (req, res, next) {
+    
+    try {
+        pool.query("select * from category", function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully' ,data:result})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+
+router.post('/submit_category',upload.any(), function (req, res, next) {
+    //req.file.filename
+    console.log("hhhhhhhhhhhhhhhhhhh:",req.body)
+    try {
+        pool.query("insert into category(categoryname, image) values(?,?)", [req.body.categoryname,req.body.picture], function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully'})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+module.exports = router;
---
+++ routes/product.js
@@ -0,0 +1,78 @@
+var express = require('express')
+var router = express.Router()
+var pool = require('./pool')
+var upload=require('./multer');
+
+router.get('/fetch_product', function (req, res, next) {
+    
+    try {
+        pool.query("select c.*,p.* from products p , category c where c.categoryid = p.categoryid", function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully' ,data:result})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+
+router.post('/submit_product',upload.any(), function (req, res, next) {
+    //req.file.filename
+    console.log("hhhhhhhhhhhhhhhhhhh:",req.body)
+    try {
+        pool.query("insert into products(categoryid,productname, picture) values(?,?,?)", [req.body.categoryid,req.body.productname,req.body.picture], function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully'})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+router.post('/fetch_product_by_categoryid',upload.any(), function (req, res, next) {
+    //req.file.filename
+    console.log("hhhhhhhhhhhhhhhhhhh:",req.body)
+    try {
+        pool.query("select * from products where categoryid =?",[req.body.categoryid], function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully',data:result})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+router.post('/submit_productdetails',upload.any(), function (req, res, next) {
+    //req.file.filename
+    console.log("hhhhhhhhhhhhhhhhhhh:",req.body)
+    try {
+        pool.query("insert into productdetailsid(categoryid, productid, color, modelno, price, offerprice, stock, rating) values(?,?,?,?,?,?,?,?)", [req.body.categoryid,req.body.productid,req.body.color,req.body.modelno,req.body.price,req.body.offerprice,req.body.stock,req.body.rating], function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully'})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+module.exports = router;
