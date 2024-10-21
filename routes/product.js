9c0aed8440ae5330fc277cf8822acb0fe6544e5e
Ajay-singh-github <ajaysikata@gmail.com>
2024-10-21 11:14:24 +0530

    added Router Fetch Data

--- routes/product.js
+++ routes/product.js
@@ -75,4 +75,39 @@ router.post('/submit_productdetails',upload.any(), function (req, res, next) {
     }
 })
 
+router.get('/fetch_categoryandproductdetails', function (req, res, next) {
+    
+    try {
+        pool.query("select * from category c, products p , productdetailsid pd  where c.categoryid=pd.categoryid and pd.categoryid = p.categoryid", function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully' ,data:result})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
+router.post('/check_admin',upload.any(), function (req, res, next) {
+    //req.file.filename
+    console.log("hhhhhhhhhhhhhhhhhhh:",req.body)
+    try {
+        pool.query("select * from admins where emailid =? and password = ?",[req.body.emailid,req.body.password], function (error, result) {
+            if (error) {
+                console.log(error)
+                res.status(200).json({ status: false, message: 'Database Error , Pls contact database Admin' })
+            }
+            else {
+                res.status(200).json({ status: true, message: 'User Data submitted successfully',data:result})
+            }
+        })
+    } catch (e) {
+        res.status(200).json({ status: false, message: 'Server Error...' })
+    }
+})
+
 module.exports = router;
